154b5f2d8b79363e9243c26db1208c4767286f0a
Narendran.Srinivasan <narendran.srinivasan@aspiresys.com>
2020-07-20 19:42:36 +0530

    GraphQL Integration

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,4 +1,5 @@
 const Article = require('../../models/blog/article');
+const Bike = require('../../models/bike/Bike');
 
 module.exports = {
   articles: async () => {
@@ -25,6 +26,51 @@ module.exports = {
       const newArticle = await article.save();
       console.log(newArticle);
       return { ...newArticle._doc, _id: newArticle.id };
+    } catch (error) {
+      throw error;
+    }
+  },
+  bikes: async () => {
+    try {
+      const bikesData = await Bike.find();
+      return bikesData.map((bike) => {
+        return {
+          ...bike._doc,
+          _id: bike.id,
+        };
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  createBike: async (args) => {
+    try {
+      const {
+        model_code,
+        model_name,
+        price_ex_shoowroom,
+        description,
+        cc,
+        no_of_cylinders,
+        max_power,
+      } = args.bike;
+
+      const data = {
+        model_code: model_code,
+        model_name: model_name,
+        price_ex_shoowroom: price_ex_shoowroom,
+        description: description,
+        engine: {
+          cc: cc,
+          no_of_cylinders: no_of_cylinders,
+          max_power: max_power,
+        },
+      };
+
+      const bike = new Bike(data);
+      const newBike = await bike.save();
+      console.log(newBike);
+      return { ...newBike._doc, _id: newBike.id };
     } catch (error) {
       throw error;
     }
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,6 +1,43 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+    type Bike {
+        _id: ID!
+        model_code: String!
+        model_name: String!
+        price_ex_shoowroom: String!
+        description: String
+        engine: BikeEngine
+        images: [Image]
+        fuel_consumptions: BikeFuel
+    }
+
+    type BikeEngine {
+        cc: String!
+        no_of_cylinders: String!
+        max_power: String!
+    }
+
+    type BikeFuel {
+        tank_capacity: String!,
+        reserve_fuel_capacity: String!,
+        mileage: String!,
+        riding_range: String!,
+    }
+
+    type Image {
+        image: String!
+    }
+
+    input BikeInput {
+        model_code: String!
+        model_name: String!
+        price_ex_shoowroom: String!
+        cc: String!
+        no_of_cylinders: String!
+        max_power: String!
+    }
+
     type Article {
         _id: ID!
         title: String!
@@ -15,10 +52,12 @@ module.exports = buildSchema(`
 
     type Query {
         articles: [Article!]
+        bikes: [Bike!]
     }
 
     type Mutation {
         createArticle(article:ArticleInput): Article
+        createBike(bike:BikeInput): Bike
     }
 
     schema {
